4259781c6f795fce320ba76e080443c3028e4c17
Geovani Geraldi <geovani.geraldi@gmail.com>
2021-04-11 21:20:18 -0300

    finish

--- front-web/src/pages/search/index.tsx
+++ front-web/src/pages/search/index.tsx
@@ -18,11 +18,20 @@ const Search = () => {
     setIsLoading(true);
     requestWeb({ usuario: busca })
       .then(response => setPerfil(response.data))
+      .catch(response => setPerfil({
+        name: '', public_repos: 0, followers: 0, following: 0,
+        company: '', blog: '', location: '', created_at: '',
+        avatar_url: '', html_url: ''
+      }))
       .finally(() => { setIsLoading(false); });
   }
 
   const handleChance = (event: React.ChangeEvent<HTMLInputElement>) => {
     setBusca(event.target.value);
+  }
+
+  const handleVerPerfil = (event: React.MouseEvent<HTMLButtonElement>) => {
+    window.open(event.currentTarget.value);
   }
 
   return (
@@ -39,53 +48,52 @@ const Search = () => {
         <br />
         <button
           className="button search-button"
-          onClick={handleRequestPerfil}
+          onClick={handleRequestPerfil}          
         >
           Encontrar
         </button>
       </div>
-      <div className="card-base">
-        {isLoading
+      
+        {isLoading  
           ? <Loaders />
           :
-          <div className="search-content-information">
-            <div>
-              <img
-                className="search-img"
-                src={perfil.avatar_url}
-                alt={perfil.name}
-              />
-              <Link
-                to={perfil.html_url}
-                target="_blank"
-              >
-                <button
-                  className="button search-button"
-                  onClick={handleRequestPerfil}
-                >
-                  Ver Perfil
-              </button>
-              </Link>
-            </div>
-            <div>
-              <div className="search-content-information">
-                <div className="search-content-top">Repositorios Publicos: {perfil.public_repos}</div>
-                <div className="search-content-top">Seguidores: {perfil.followers}</div>
-                <div className="search-content-top">Seguindo: {perfil.following}</div>
+          ((perfil.html_url) && 
+          <div className="card-base">
+            <div className="search-content-information">
+              <div>
+                <img
+                  className="search-img"
+                  src={perfil.avatar_url}
+                  alt={perfil.name}
+                />
+                  <button
+                    className="button search-button"
+                    onClick={handleVerPerfil}
+                    value={perfil.html_url}
+                  >
+                    Ver Perfil
+                </button>
               </div>
-              <div className="search-content-information">
-                <div className="search-content-group">
-                  <h4 className="search-text-information">Informações</h4>
-                  <div className="search-content"><strong>Empresa:</strong> {perfil.company}</div><br/>
-                  <div className="search-content"><strong>Website/Blog:</strong> {perfil.blog}</div><br/>
-                  <div className="search-content"><strong>Localidade:</strong> {perfil.location}</div><br/>
-                  <div className="search-content"><strong>Membro desde:</strong> {perfil.created_at}</div>
+              <div>
+                <div className="search-content-information">
+                  <div className="search-content-top">Repositorios Publicos: {perfil.public_repos}</div>
+                  <div className="search-content-top">Seguidores: {perfil.followers}</div>
+                  <div className="search-content-top">Seguindo: {perfil.following}</div>
+                </div>
+                <div className="search-content-information">
+                  <div className="search-content-group">
+                    <h4 className="search-text-information">Informações</h4>
+                    <div className="search-content"><strong>Empresa:</strong> {perfil.company}</div><br/>
+                    <div className="search-content"><strong>Website/Blog:</strong> {perfil.blog}</div><br/>
+                    <div className="search-content"><strong>Localidade:</strong> {perfil.location}</div><br/>
+                    <div className="search-content"><strong>Membro desde:</strong> {perfil.created_at}</div>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        }
-      </div>
+          </div>    
+          )
+          }      
     </div>
   );
 }
